871ea51f3b67e5beadea1ed28925f59163ff1f66
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-01-24 09:03:54 +0000

    Added some JS functionality
    App can now accept input value and make calculations of the length.
    Tried to make one function, that would work for all three instances.
    Need to add function to calculate the remaining units.

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-let inputEl = document.getElementById("input-el")
-inputEl.value ="";
+const convertEl = document.getElementById("convert-el")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const weightEl = document.getElementById("weight-el")
 
 
 
-var toggle = document.getElementById("theme-toggle");
-
-var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-if (storedTheme)
-    document.documentElement.setAttribute('data-theme', storedTheme)
-
-
-toggle.onclick = function() {
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    var targetTheme = "light";
-
-    if (currentTheme === "light") {
-        targetTheme = "dark";
+function calculation(inputValue, dimensionRatio) {
+    multipliedValue = inputValue * dimensionRatio
+    dividedValue = inputValue / dimensionRatio
+    //return multipliedValue, dividedValue 
+    console.log(multipliedValue, dividedValue)
     }
 
-    document.documentElement.setAttribute('data-theme', targetTheme)
-    localStorage.setItem('theme', targetTheme);
-};
+convertEl.addEventListener("click", function() {
+    let inputEl = document.getElementById("input-el").value
+    calculation(inputEl, 3.281)
+    lengthEl.innerHTML = `<p>${inputEl} meters = ${multipliedValue.toFixed(3)} feet | ${inputEl} feet = ${dividedValue.toFixed(3)} meters</p>`
+
+})
+
+
+// var toggle = document.getElementById("theme-toggle");
+
+// var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+// if (storedTheme)
+//     document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+// toggle.onclick = function() {
+//     var currentTheme = document.documentElement.getAttribute("data-theme");
+//     var targetTheme = "light";
+
+//     if (currentTheme === "light") {
+//         targetTheme = "dark";
+//     }
+
+//     document.documentElement.setAttribute('data-theme', targetTheme)
+//     localStorage.setItem('theme', targetTheme);
+// };
