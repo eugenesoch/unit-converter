0b56ad3341ba9f31b396ddaee4d44c00dee43c10
Jevgenij Soch <110429125+eugenesoch@users.noreply.github.com>
2024-01-24 07:24:26 +0000

    Further works on design
    Continued working on the layout of the app - applied font sizes, button shape and size, started planning JS code.

--- README.md
+++ README.md
@@ -7,11 +7,40 @@ Follow the design spec on Figma
 Generate all conversions when user clicks "Convert"
 Round the numbers down to three decimal places
 
-/*
+/* metric to imperial
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+/* us to metric
+1 feet = 0.3047 meter
+1 gallon = 3.7878 litres
+1 pound = 0.4537 kilogram
+*/
+
+
+Breakdown of the functions:
+
+1 Need to read a value of entered number.
+2 Store it in a variable inputNum
+3 Pass the variable to the function to perform calculations required. 
+
+Conversion values
+
+Length 3.281 - variable lengthRatio
+Volume 0.264 - variable volumeRatio
+Weight 2.204 - variable weightRadio
+
+Calculation 
+
+inputNum * dimensionRatio
+inputNum / dimensionRatio
+
+
+Calculations
+
+
 
 //help section url: https://projects.scrimba.com/unit-converter
 
--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-let passwordEl1 = document.getElementById("password-el1")
-let passwordEl2 = document.getElementById("password-el2")
-let buttonEl = document.getElementById("button-el")
-passwordEl1.textContent=("****************")
-passwordEl2.textContent=("****************")
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+let inputEl = document.getElementById("input-el")
+inputEl.textContent=("Bravo")
 
-let passwordLength = 15
 
-function getRandomCharacter() {
-    let randomChar = Math.floor(Math.random() * characters.length)
-    return characters[randomChar]
-}
-
-function generatePassword() {
-    let randomPassword = []
-    for (let i = 0; i < passwordLength; i++)
-    randomPassword += getRandomCharacter()
-    return randomPassword
-}
-
-function generateRandomPasswords() {
-    const password1 = generatePassword()
-    passwordEl1.textContent=(password1)
-    const password2 = generatePassword()
-    passwordEl2.textContent=(password2)
-    buttonEl.textContent=("Generate Again")
-
-}
 
 var toggle = document.getElementById("theme-toggle");
 
